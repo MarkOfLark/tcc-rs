6ba36aa91a8c3a8baf621d0f131e956278e53fe4
Mark Elias McDermott <mark.elias.mcdermott@gmail.com>
2015-01-25 23:08:25 -0500

    start exploring implementation possibilities

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#![crate_name = "tcc"]
+#![crate_type = "lib"]
+
+#[test]
+fn it_works() {
+}
+
+
+struct Date {
+    year: i64,
+    month: i8,
+    day: i8,
+    hour: i8,
+    minute: i8,
+    second: i8,
+    nano_second: i32,
+    year_base: u64,
+    mod_quarter: i64,
+    mod_month: i64,
+    mod_week: i64,
+    mod_day: i64,
+    mod_hour: i64,
+    mod_minute: i64,
+    mod_second: i64,
+}
