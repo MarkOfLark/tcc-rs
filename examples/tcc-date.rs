5d5778803d51489cbd4ddc8257d5b34eb0a8c037
Mark Elias McDermott <mark.elias.mcdermott@gmail.com>
2015-01-26 01:37:29 -0500

    implement basic plumbing

--- examples/tcc-date.rs
+++ examples/tcc-date.rs
@@ -1,8 +1,36 @@
 extern crate tcc;
-extern crate time;
+extern crate getopts;
+use getopts::{optopt,optflag,getopts,OptGroup,usage};
 
 fn main() {
-    let ts = time::get_time();
-    let t = ts.sec as f64 + 1.0e-9*(ts.nsec as f64);
+    // Get time right away so that printed time does not reflect
+    // time spent parsing arguments and formatting
+    let t = tcc::now();
+
+    // Get options from command line arguments
+    let opts = &[
+        optflag("h", "help", "print this help menu"),
+        optopt("y","year-base","Integer representing the year base","NUMBER"),
+        optopt("Q","mod-quarter","Integer representing the datemod for quarters","NUMBER"),
+        optopt("L","mod-luna","Integer representing the datemod for lunas","NUMBER"),
+        optopt("W","mod-week","Integer representing the datemod for weeks","NUMBER"),
+        optopt("D","mod-day","Integer representing the datemod for days","NUMBER"),
+        optopt("H","mod-hour","Integer representing the datemod for hours","NUMBER"),
+        optopt("M","mod-minute","Integer representing the datemod for minutes","NUMBER")
+    ];
+   
+
+    let matches = match getopts(std::os::args().tail(), opts) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print!("{}", usage("tcc-date", opts));
+        println!("\nExample: tcc-date -h 5");
+        return;
+    }
+
     println!("{}",t);
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
+
 #![crate_name = "tcc"]
 #![crate_type = "lib"]
+
+extern crate time;
 
 #[test]
 fn it_works() {
 }
 
 
-struct Date {
+pub struct TCDate {
     year: i64,
     month: i8,
     day: i8,
@@ -22,4 +25,49 @@ struct Date {
     mod_hour: i64,
     mod_minute: i64,
     mod_second: i64,
+}
+
+pub fn now() -> TCDate{
+    let utc = time::get_time();
+
+    let mut ts : i64 = utc.sec -864000;
+    let year = ts / (60*60*24*28*13);
+    ts = ts - year*13*28*24*60*60;
+    let mon = ts / (60*60*24*28);
+    ts = ts - mon*28*24*60*60;
+    let day = ts / (60*60*24);
+    ts = ts - day*24*60*60;
+    let hour = ts / (60*60);
+    ts = ts - hour*60*60;
+    let min = ts / 60;
+    ts = ts - min*60;
+
+    TCDate{year: year as i64,
+           month: mon as i8,
+           day: day as i8,
+           hour: hour as i8,
+           minute: min as i8,
+           second: ts as i8,
+           nano_second: utc.nsec as i32,
+           year_base: 0u64,
+           mod_quarter: 0i64,
+           mod_month: 0i64,
+           mod_week: 0i64,
+           mod_day: 0i64,
+           mod_hour: 0i64,
+           mod_minute: 0i64,
+           mod_second: 0i64}
+}
+
+impl std::fmt::Display for TCDate {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f,"{}.{}.{},{}.{}:{} TC{}",
+               self.year,
+               self.month,
+               self.day,
+               self.hour,
+               self.minute,
+               self.second as f64 + 1e-9*(self.nano_second as f64),
+               self.year_base)
+    }
 }
